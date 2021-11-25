fb1fed17b70958bc1a5cef0df8e0953fadad1eb5
moritzblum <blum-moritz@t-online.de>
2021-11-25 17:15:26 +0100

    added missing cards and restructured some existing cards

---
+++ app_form.js
@@ -0,0 +1,36 @@
+console.log("loaded: app_form.js")
+
+
+var datepicker_german = {
+    currentText: 'heute', currentStatus: '',
+    todayText: 'heute', todayStatus: '',
+    clearText: '-', clearStatus: '',
+    closeText: 'schließen', closeStatus: '',
+    monthNames: ['Januar','Februar','März','April','Mai','Juni',
+        'Juli','August','September','Oktober','November','Dezember'],
+    monthNamesShort: ['Jan','Feb','Mär','Apr','Mai','Jun',
+        'Jul','Aug','Sep','Okt','Nov','Dez'],
+    dayNames: ['Sonntag','Montag','Dienstag','Mittwoch','Donnerstag','Freitag','Samstag'],
+    dayNamesShort: ['So','Mo','Di','Mi','Do','Fr','Sa'],
+    dayNamesMin: ['So','Mo','Di','Mi','Do','Fr','Sa'],
+    showMonthAfterYear: false,
+    dateFormat:'d MM, yy'
+};
+
+$( function() {
+    $("#datepicker" ).datepicker(datepicker_german);
+});
+
+
+$(function() {
+    $("#datepicker_impftermin").datepicker(datepicker_german);
+});
+
+$(function() {
+    $("#datepicker_infection").datepicker(datepicker_german);
+});
+
+$(function() {
+    $("#datepicker_symptom_free").datepicker(datepicker_german);
+});
+
