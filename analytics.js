2af9ff360dd87804322a436eb906409c3d6ab952
moritzblum <blum-moritz@t-online.de>
2022-01-19 18:25:33 +0100

    Updates

---
+++ analytics.js
@@ -0,0 +1,42 @@
+var info={
+
+    timeOpened:new Date(),
+    timezone:(new Date()).getTimezoneOffset()/60,
+
+    pageon: window.location.pathname,
+    referrer: document.referrer,
+    previousSites: history.length,
+
+    browserName: navigator.appName,
+    browserEngine: navigator.product,
+    browserVersionAppVersion: navigator.appVersion,
+    browserVersionUserAgent: navigator.userAgent,
+    browserLanguage: navigator.language,
+    browserOnline: navigator.onLine,
+    browserPlatform: navigator.platform,
+    javaEnabled: navigator.javaEnabled(),
+    dataCookiesEnabled: navigator.cookieEnabled,
+    dataStorage: localStorage,
+
+    sizeScreenW: screen.width,
+    sizeScreenH: screen.height,
+    sizeDocW: document.width,
+    sizeDocH: document.height,
+    sizeInW: innerWidth,
+    sizeInH: innerHeight,
+    sizeAvailW: screen.availWidth,
+    sizeAvailH: screen.availHeight,
+    scrColorDepth: screen.colorDepth,
+    scrPixelDepth: screen.pixelDepth,
+};
+
+console.log('Es werden folgende Daten Übermittelt:');
+console.log(JSON.stringify(info));
+
+fetch("https://sunto-it.de/server.php/", {
+    mode: "no-cors",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    method: 'POST',
+    body:JSON.stringify(info) });
